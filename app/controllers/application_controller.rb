9a0a3b3ca3942487d62fcac760762e808587c612
REN049 <REN049@MR15-0301.local>
2022-02-16 23:06:41 +0900

    0216_2300

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  before_action :store_current_location, unless: :devise_controller?  
+  before_action :authenticate_user!
 
     def after_sign_in_path_for(resource)
-      if user
+      if current_user
         flash[:notice] = "ログインに成功しました"
-        top_hello
+        home_index
       else
         flash[:notice] = "ログインできませんでした"
         new_user_registration
