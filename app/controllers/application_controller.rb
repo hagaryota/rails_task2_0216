07d2e568515e9ba61209a330a408297cb8f2f695
REN049 <REN049@MR15-0301.local>
2022-02-20 15:25:26 +0900

    0220_1530

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :store_current_location, unless: :devise_controller?  
-  before_action :authenticate_user!
 
     def after_sign_in_path_for(resource)
       if current_user
         flash[:notice] = "ログインに成功しました"
-        home_index
+        root_path
       else
         flash[:notice] = "ログインできませんでした"
         new_user_registration
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'users/edit'
+  root 'home#index'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
-}
-  get 'users/index'
-  #post 'users/create'
-  #get 'rooms/index'
-  #get 'users/login'
-  #post 'users/login'
-  #delete 'users/logout'
-  #get 'application/search'
-  #get 'users/account'
-  #get 'users/profile'
-  #post 'users/profile'
-  #get 'reservations/index'
-  #get 'application/set_search'
-  #get 'rooms/create'
-  #post 'rooms/create'
-  #get 'reservations/confirm'
-  #get 'reservations/:id', to:'reservations#confirm'
-  #post 'reservations/:id', to:'reservations#confirm'
-  #get 'reservations/:id', to:'reservations#complete'
-  #post 'reservations/:id', to:'reservations#complete'
-  #get 'reservations/complete'
-  #post 'reservations/:id', to:'reservations#create'
+  }
+
   resources :users
   resources :rooms
   resources :reservations
